c80cb92dc1a9fa739fc3b7079fd7e6efe9765a5b
maq-ahmad-20 <Maqboolahmad081@gmail.com>
2023-11-05 16:04:25 +0530

    dynamicdisplayofcartbuton

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,17 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from './headerCartbutton.module.css';
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../Store/Cart-context";
 const HeaderCartButton = (props) => {
 
-    const cartContext = useContext(CartContext);
+    const [btnIsHighlited, setBtnIsHighlited] = useState(false)
 
+    const cartContext = useContext(CartContext);
+    const { items } = cartContext;
     const noOfCartItems = cartContext.items.reduce((len, item) => {
         return len += item.amount;
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlited ? classes.bump : ''}`
+
+
+    useEffect(() => {
+        if (items.length === 0) return;
+        setBtnIsHighlited(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlited(false);
+        }, 300)
+
+        return () => {
+            clearTimeout(timer)
+        }
+
+    }, [items])
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
